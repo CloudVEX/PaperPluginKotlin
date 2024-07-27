6eef031720464245e9c8e2ff4df5c81b23a0e3e6
Cloud <cloudvexwastaken@gmail.com>
2024-07-28 01:35:55 +0200

    switch to truly using paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,23 +3,34 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 plugins {
     kotlin("jvm") version "2.+"
     id("io.papermc.paperweight.userdev") version "1.7.+"
-    id("xyz.jpenilla.run-paper") version "2.2.2" // newer version does not work currently
-    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("net.minecrell.plugin-yml.paper") version "0.6.0"
+    id("com.github.gmazzo.buildconfig") version "5.4.0"
 }
 
 val serverVersion: String by project
 
 val packageString = "${properties["group"] as String}.${rootProject.name.lowercase()}"
 
+val kSpigotVersion: String by project
+
 repositories {
     mavenCentral()
+    maven {
+        name = "laturia"
+        url = uri("https://repo.laturia.net/releases")
+        credentials(PasswordCredentials::class)
+        authentication {
+            create<BasicAuthentication>("basic")
+        }
+    }
 }
 
 dependencies {
     paperweight.paperDevBundle("$serverVersion-R0.1-SNAPSHOT")
 
     // Minecraft
-    library("net.axay:kspigot:1.20.4")
+    library("net.axay:kspigot:$kSpigotVersion")
 
     // Kotlin
     implementation(kotlin("stdlib"))
@@ -27,22 +38,31 @@ dependencies {
     library("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.+")
 }
 
-bukkit {
+buildConfig {
+    className("BuildConfig")
+    packageName(packageString)
+
+    buildConfigField("KSPIGOT_VERSION", kSpigotVersion)
+}
+
+paper {
+    group = properties["group"] as String
+    main = "$packageString.Main"
+
+    loader = "$packageString.loader.CustomLoader"
 
     name = properties["name"] as String
     author = properties["author"] as String
     description = properties["description"] as String
     website = properties["website"] as String
 
-    group = properties["group"] as String
-    main = "$packageString.Main"
+    generateLibrariesJson = true
 
     version = properties["version"] as String
-    apiVersion = "1.20"
+    apiVersion = "1.21"
 
-    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
-    depend = listOf()
-    softDepend = listOf()
+    load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
+    serverDependencies {}
 }
 
 kotlin {
